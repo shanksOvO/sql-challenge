1d949b8e1ee76a04fb2bb661e810b3bc5b52346c
shanksOvO <128906024+shanksOvO@users.noreply.github.com>
2023-06-07 22:46:14 -0400

    Add files via upload

---
+++ EmployeeSQL/Table_Schema.sql
@@ -0,0 +1,47 @@
+-- Create tbale departments
+CREATE TABLE departments(
+	dept_no VARCHAR NOT NULL PRIMARY KEY,
+	dept_name VARCHAR NOT NULL
+);
+
+-- Create table title
+CREATE TABLE title(
+	title_id VARCHAR NOT NULL PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
+
+-- Create table employees
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES title(title_id)
+);
+
+-- Create table dept_emp
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- Create table dept_manager
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Create table salaries
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
